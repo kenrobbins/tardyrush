e58eaf413b4784569b1ae85d59b5ba32111d68c7
Ken Robbins <kenneth.robbins@gmail.com>
2011-05-01 18:49:36 -0400

    Start organizing the code.

--- tardyrush/__init__.py
+++ tardyrush/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flaskext.sqlalchemy import SQLAlchemy
+from flaskext.openid import OpenID
+from flaskext.mail import Mail
+from flaskext.babel import Babel
 
 app = Flask(__name__)
 app.config.from_pyfile('tardyrush.cfg')
 
 db = SQLAlchemy(app)
+oid = OpenID(app)
+babel = Babel(app)
+mail = Mail(app)
 
 def init_db():
     db.create_all()
@@ -12,5 +18,21 @@ def init_db():
 def destroy_db():
     db.drop_all()
 
+from tardyrush.views.teams import teams
+from tardyrush.views.matches import matches
+from tardyrush.views.team_admin import team_admin
+from tardyrush.views.account import account
+from tardyrush.views.results import results
+
+app.register_module(teams)
+app.register_module(matches)
+app.register_module(team_admin)
+app.register_module(account)
+app.register_module(results)
+
+# app filters
+# helpers
+
 import tardyrush.models
 import tardyrush.views
+
